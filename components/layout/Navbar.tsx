c74497beab373c1b862bb02227ef827547ce6a7d
Greyman <psbalajvlr@gmail.com>
2026-07-31 12:16:20 +0100

    Complete portfolio redesign and UI enhancement

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -93,7 +93,7 @@ export default function Navbar() {
             tracking-wide
           "
         >
-          <span className="text-blue-600">B</span>alaj
+          Balaj
         </a>
 
         {/* Desktop Navigation */}
